4e711c064bdbc1da5e12eb6d4d1381630579ad6f
Gianpaolo <valerogian@gmail.com>
2014-09-06 12:52:07 +0100

    First commit with picture model created

---
+++ app/models/picture.rb
@@ -0,0 +1,17 @@
+class Picture
+
+  include DataMapper::Resource
+  include Paperclip::Resource
+
+  property :id, Serial
+
+  property :title,  String,   :required => true
+  property :descr,  Text,     :required => true
+  #property :path,   FilePath
+  #property :md5sum, String,   :length => 32, :default => lambda { |r, p| Digest::MD5.hexdigest(r.path.read) if r.path }
+  #property :created_at, DateTime
+
+  has_attached_file :image, :styles => {:medium => "300x300", :thumb => "100x100"}
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElasticPicture::Application.config.secret_token = '6b814c288262daca49a9bcfe754f8b66a128373967fab99890ce9932759238beec357f3d48c9a2e210b9259b0d32061266b3b0989c13723dda0adbf861c25d3c'
